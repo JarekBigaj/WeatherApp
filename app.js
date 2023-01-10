ef039af52fbf8f427a2fbea898df0bc7a450979a
Jarek Bigaj <j.bigaj@interia.pl>
2023-01-10 20:24:35 +0100

    display data of weather on the page

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ krakowButton.addEventListener('click', () =>{
     fetch(apiUrlKrakow).then(response => response.json())
     .then(response => {
         const {current_weather} = response;
+        displayWeatherData(current_weather);
     });
 });
 
@@ -17,6 +18,7 @@ warsawButton.addEventListener('click', () =>{
     fetch(apiUrlWarsaw).then(response => response.json())
     .then(response => {
         const {current_weather} = response;
+        displayWeatherData(current_weather);
     });
 });
 
@@ -24,5 +26,58 @@ marcowkaButton.addEventListener('click', () =>{
     fetch(apiUrlMarcowka).then(response => response.json())
     .then(response => {
         const {current_weather} = response;
+        displayWeatherData(current_weather);
     });
 });
+
+const displayWeatherData = (currentWeather) =>{
+    const timeLabel = document.querySelector('[data-id="time"]')
+    const tempLabel = document.querySelector('[data-id="temperature"]');
+    const windSpeedLabel = document.querySelector('[data-id="windSpeed"]');
+    const weatherCodeLabel = document.querySelector('[data-id="weatherCode"]');
+
+    const {temperature,time,weathercode,winddirection,windspeed} = currentWeather;
+
+    timeLabel.textContent = time;
+    tempLabel.textContent = temperature;
+    windSpeedLabel.textContent = windspeed;
+    const informationAboutCurrentWeather = Object.entries(weatherCodeList).find((weather,index) => {
+        const [code,informationAboutWeather] = weather;
+        if(parseInt(code) === weathercode){
+            return informationAboutWeather
+        }
+    })
+    weatherCodeLabel.textContent = informationAboutCurrentWeather;
+    
+}
+
+const weatherCodeList = {
+    0 : "Clear sky",
+    1 : "Mainly clear, partly cloudy, and overcast",
+    2 : "Mainly clear, partly cloudy, and overcast", 
+    3 : "Mainly clear, partly cloudy, and overcast",
+    45 : "Fog and depositing rime fog", 
+    48 : "Fog and depositing rime fog",
+    51 : "Drizzle: Light, moderate, and dense intensity",
+    53 : "Drizzle: Light, moderate, and dense intensity", 
+    55 : "Drizzle: Light, moderate, and dense intensity",
+    56 : "Freezing Drizzle: Light and dense intensity",
+    57 : "Freezing Drizzle: Light and dense intensity",
+    61 : "Rain: Slight, moderate and heavy intensity",
+    63 : "Rain: Slight, moderate and heavy intensity",
+    65 : "Rain: Slight, moderate and heavy intensity",
+    66 : "Freezing Rain: Light and heavy intensity",
+    67 : "Freezing Rain: Light and heavy intensity",
+    71 : "Snow fall: Slight, moderate, and heavy intensity",
+    73 : "Snow fall: Slight, moderate, and heavy intensity",
+    75 : "Snow fall: Slight, moderate, and heavy intensity",
+    77 : "Snow grains",
+    80 : "Rain showers: Slight, moderate, and violent",
+    81 : "Rain showers: Slight, moderate, and violent", 
+    82 : "Rain showers: Slight, moderate, and violent",
+    85 : "Snow showers slight and heavy", 
+    86 : "Snow showers slight and heavy",
+    95 : "Thunderstorm: Slight or moderate",
+    96 : "Thunderstorm with slight and heavy hail",
+    99 : "Thunderstorm with slight and heavy hail"
+}
