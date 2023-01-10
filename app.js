7484dac4ee0e43fae75d1156a96295af049ec482
Jarek Bigaj <j.bigaj@interia.pl>
2023-01-10 19:02:17 +0100

    create view application

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const apiUrlKrakow = 'https://api.open-meteo.com/v1/forecast?latitude=50.06&longitude=19.94&hourly=temperature_2m,precipitation,rain,showers,snowfall';
-const apiUrlWarsaw ='https://api.open-meteo.com/v1/forecast?latitude=52.23&longitude=21.01&hourly=temperature_2m,precipitation,rain,showers,snowfall';
-const apiUrlMarcowka ='https://api.open-meteo.com/v1/forecast?latitude=49.79&longitude=19.62&hourly=temperature_2m,precipitation,rain,showers,snowfall';
+const apiUrlKrakow = 'https://api.open-meteo.com/v1/forecast?latitude=50.06&longitude=19.94&current_weather=true';
+const apiUrlWarsaw ='https://api.open-meteo.com/v1/forecast?latitude=52.23&longitude=21.01&current_weather=true';
+const apiUrlMarcowka ='https://api.open-meteo.com/v1/forecast?latitude=49.79&longitude=19.62&current_weather=true';
 
 
 fetch(apiUrlKrakow).then(response => response.json())
 .then(response => {
-    const city = document.querySelector("#city");
-    const latitude = response.latitude;
-    const longitude =response.longitude;
+    console.log(response);
 });
